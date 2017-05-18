c754197ad9cfa6e9e465834798a2ad02d10ee97b
creder <creder@cisco.com>
2017-05-18 11:31:53 -0400

    added tech-support

---
+++ bin/save-tech-support-to-s3.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from csr_aws_guestshell import cag
+import argparse
+
+parser = argparse.ArgumentParser(description="Upload tech-support to S3")
+parser.add_argument('bucket', help='The name of the bucket to upload to')
+parser.add_argument('filename', help='Filename to upload to bucket')
+args = parser.parse_args()
+
+bucket = args.bucket
+filename = args.filename
+
+cag().save_cmd_output(["show tech-support"], filename, bucket)
+# with open('/bootflash/' + filename, 'w') as f:
+#     f.write(cli.execute("show tech-support"))
+
+# cag().upload_file(bucket, filename)
