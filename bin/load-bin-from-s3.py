1ca44c342d8d69929b2ed243c96f12c0a48e1e33
creder <creder@cisco.com>
2017-05-18 12:41:49 -0400

    updated to load bin file

---
+++ bin/load-bin-from-s3.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import cli
+from csr_aws_guestshell import cag
+import argparse
+
+parser = argparse.ArgumentParser(description="Upload config file")
+parser.add_argument('bucket', help='The name of the bucket to upload to')
+parser.add_argument('filename', help='Filename to upload to bucket')
+parser.add_argument('--reload', help='After downloading, reboot system', default=None)
+args = parser.parse_args()
+
+bucket = args.bucket
+filename = args.filename
+
+cag().download_file(bucket, filename)
+
+configuration = "boot system flash:" + filename
+cli.configure(configuration)
+cli.execute("copy running start")
+if args.reload is not None:
+    cli.execute("reload")
