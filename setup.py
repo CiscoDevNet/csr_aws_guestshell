dc3829a9506413dac08088c9438ed69d1f47cd6e
creder <creder@cisco.com>
2017-11-09 22:04:54 -0500

    updated version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 project_name = 'csr_aws_guestshell'
-project_ver = '0.0.6.dev'
+project_ver = '0.0.8.dev'
 setup(
     name=project_name,
     packages=[project_name],  # this must be the same as the name above
